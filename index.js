4843fc7e16f462ec0e6930374c5add97b86e13d4
caio7css <1caioccss@gmail.com>
2024-12-13 16:28:10 -0300

    meu primeiro desafio da DIO

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-blablabla
+var nome = 'caio'
+
+var nivel = '5555'
+
+if (nivel < 1000) {
+nivel = 'Ferro'
+}
+
+else if (nivel >= 1001 && nivel <= 2000) {
+    nivel = 'Bronze'
+}
+
+else if (nivel >= 2001 && nivel <= 5000) {
+    nivel = 'Prata'
+}
+
+else if (nivel >= 5001 && nivel <=7000) {
+    nivel = 'Ouro'
+}
+
+else if (nivel >= 7001 && nivel <= 8000) {
+    nivel = 'Platina'
+}
+
+else if (nivel >= 8001 && nivel <= 9000){
+    nivel = 'Ascendente'
+}
+
+else if (nivel >= 9001 && nivel <= 10000){
+    nivel = 'Imortal'
+}
+
+else if (nivel >= 10001){
+    nivel ='Radiante'
+}
+
+console.log('O herói ' + nome , 'está no rank ' + nivel )
